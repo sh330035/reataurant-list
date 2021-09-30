7172099a39f0730e27363c4462f08115f7da2109
david3335 <sh330035@gmail.com>
2021-09-30 20:45:30 +0800

    add search function & add the comment in app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+// include modules
 const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
+
+// include restaurants data from JSON
 const restaurantsList = require('./restaurant.json')
 const restaurants = restaurantsList.results
 
+// port variation
 const port = 3000
 
 // 使用 handlebars
@@ -14,14 +18,28 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 // setting route
+// 首頁 route
 app.get('/', (req, res) => {
   res.render('index', { restaurants })
 })
 
-app.get('/search/', (req, res) => {
-  res.render('index', { restaurants })
+// search route
+app.get('/search', (req, res) => {
+  const keywords = req.query.keywords
+  const restaurantSearch = restaurants.filter((restaurant) => {
+    // 可從名字、英文名字、種類、描述來搜尋餐廳
+    if (
+      restaurant.name.toLowerCase().includes(keywords.toLowerCase()) ||
+      restaurant.name_en.toLowerCase().includes(keywords.toLowerCase()) ||
+      restaurant.category.includes(keywords) ||
+      restaurant.description.toLowerCase().includes(keywords.toLowerCase())
+    )
+      return restaurant
+  })
+  res.render('index', { restaurants: restaurantSearch, keywords })
 })
 
+// show route
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurantId = req.params.restaurant_id
   const restaurant = restaurants.find(
